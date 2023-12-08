084a2956f78662517c53bd4fe4629f5e05acc022
unknown <nnmthu08@gmail.com>
2023-12-08 14:21:29 +0700

    SIGNUP

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import { RouterProvider } from 'react-router'
 import { GlobalStyle } from './conponents/global-style/global-style'
 import Login from "../src/pages/login/login"
-
+import Register from "../src/pages/register/register"
 
 function App() {
 
   return (
     <GlobalStyle>
-      <Login/>
+     <Register/>
     </GlobalStyle>
   )
 }
--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,11 +1,114 @@
-import React from 'react'
+import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+import { Col, Row } from "antd";
+import * as S from "./style";
+import Logo from "../../assets/icons/logo";
 
-function register() {
+function Register() {
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  type FieldType = {
+    username?: string;
+    password?: string;
+    remember?: string;
+  };
+
   return (
-    <div>
-      register
-    </div>
-  )
+    <S.Div>
+      <Row>
+        <S.ColLeft span={12}>
+          <S.DivContent>
+          <p style={{
+            fontSize: '3rem',
+            fontWeight:'700'
+          }}>
+            Glad to see you!
+            </p>
+
+
+          <S.Logo>
+          <Logo/>
+          </S.Logo>
+
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, minima!</p>
+          </S.DivContent>
+         </S.ColLeft>
+
+         
+          
+
+        <S.ColRight span={12}>
+          <div style={{textAlign:'center', marginBottom:'1rem'}}>
+          <p style={{
+            fontSize: '3rem',
+            fontWeight:'700'
+          }}>Hello, friend!</p>
+
+          </div>
+
+        
+          <S.FormIn
+            name="basic"
+            // labelCol={{ span: 10 }}
+            // wrapperCol={{ span: 10 }}
+            // style={{ maxWidth: 500 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item<FieldType>
+              // label="Email"
+              name="Email"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input placeholder="Email" />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              // label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password placeholder="Password" />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{ offset: 0, span: 16 }}
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 4, span: 16 }} style={{textAlign:'center', marginBottom:'1rem'}}>
+              <S.ButtonIn htmlType="submit">
+                CREATE ACCOUNT
+              </S.ButtonIn>            
+            </Form.Item>
+            <div
+              style={{
+                display:'flex',
+                justifyContent:'center'}}>
+              <p>Create account</p>
+              <a>Create</a>
+
+              </div>
+          </S.FormIn>
+        </S.ColRight>
+      </Row>
+    </S.Div>
+  );
 }
 
-export default register;
+export default Register;
